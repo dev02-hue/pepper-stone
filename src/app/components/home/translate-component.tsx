f890e34a76a1cd337eba3286c02673143f160080
lordgreg003 <chideranwokoye555@gmail.com>
2025-04-17 12:18:50 +0100

    feat:hero and nav component

---
+++ src/app/components/home/translate-component.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Translate from '../Translate'
+
+ const Translatecomponent = () => {
+  return (
+    <div className=" ">
+  <Translate />
+</div>
+
+  )
+}
+
+export default Translatecomponent;
+ 
